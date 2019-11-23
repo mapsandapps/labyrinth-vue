5a895afd95e3d2de1a34e03a80e9309909e86734
Shawn Taylor <shawn@mapsandapps.net>
2019-11-22 19:51:17 -0500

    CSS-only way!

--- src/components/Labyrinth.vue
+++ src/components/Labyrinth.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div>
     <svg
       width="500"
       height="500"
@@ -10,53 +10,82 @@
       @mouseout="mouseout"
       @mousemove="mousemove">
       <path
+        class="outline"
+        stroke="#546e7a"
+        stroke-width="30"
+        fill="none"
+        :d="pathD" />
+      <path
+        id="animated-path"
+        class="path"
+        :style="animatedPathStyles"
         :d="pathD"
         stroke="#B58A47"
         stroke-width="20"
         fill="none" />
-      <circle r="20" transform="translate(256, 384)" />
+      <circle
+        class="marker"
+        :style="animatedMarkerStyles"
+        r="4"
+        fill="orange" />
     </svg>
-    <button id="button" @click="transition">Restart</button>
+    <span v-if="finished">Done!</span>
+    <!-- <button id="button" @click="transition">Restart</button> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
-import * as d3 from 'd3'
 
-// help from https://bl.ocks.org/mbostock/1705868
+// TODO: remove d3 and d3-path if this works
 
 @Component
 export default class Labyrinth extends Vue {
   pathD: string = 'M 512,64 a 192,192 0 0,1 192,192 a 192,192 0 0,1 -192,192 a 256,256 0 0,0 256,-256 a 320,320 0 0,1 -320,320 a 192,192 0 0,1 -192,-192 a 128,128 0 0,0 128,128 a 64,64 0 0,0 64,-64 a 64,64 0 0,1 64,-64 a 64,64 0 0,0 64,-64 a 64,64 0 0,0 -64,-64 a 64,64 0 0,0 -64,64 a 64,64 0 0,1 -64,64 a 128,128 0 0,1 -128,-128'
 
   moving: boolean = false
-
-
+  pathLength: number = 0
   SPEED_MULTIPLIER: number = 4
-
   maxSpeed: number = 0
+  finished: boolean = false
 
-  circle = d3.select('circle')
+  animatedPathStyles: string = 'animation-play-state: paused'
+  animatedMarkerStyles: string = 'animation-play-state: paused;'
 
-  path = d3.select('path')
+  // get cssVars () {
+  //   return {
+  //     '--path-length': this.pathLength,
+  //     // more
+  //   }
+  // }
+
+  calculatePathStyles(running) {
+    return `
+      --path-length: ${this.pathLength};
+      animation-play-state: ${running? 'running' : 'paused'};
+    `
+  }
+
+  calculateMarkerStyles(running) {
+    return `
+      animation-play-state: ${running? 'running' : 'paused'};
+      motion-path: path('${this.pathD}');
+      offset-path: path('${this.pathD}');
+    `
+  }
 
   mousedown() {
     console.log('mousedown')
     this.moving = true
-    this.circle.transition()
-      .duration(this.maxSpeed)
-      .attrTween("transform", this.translateAlong(this.path.node()))
-      .ease(d3.easeLinear)
-    // TODO: http://xaedes.de/dev/transitions/
-    // or maybe better yet the spiral example here: https://observablehq.com/@lemonnish/svg-path-animations-d3-transition
+    this.animatedPathStyles = this.calculatePathStyles(this.moving)
+    this.animatedMarkerStyles = this.calculateMarkerStyles(this.moving)
   }
 
   mouseup() {
     console.log('mouseup')
     this.moving = false
-    this.circle.transition()
-      .duration(0)
+    this.animatedPathStyles = this.calculatePathStyles(this.moving)
+    this.animatedMarkerStyles = this.calculateMarkerStyles(this.moving)
   }
 
   mouseout() {
@@ -71,39 +100,63 @@ export default class Labyrinth extends Vue {
     console.log('mousemove')
   }
 
+  listener(e) {
+    console.log(e)
+    if (e.type === 'animationend') {
+      this.finished = true
+    }
+  }
+
   mounted() {
-    this.path = d3.select('path')
-    this.circle = d3.select('circle')
+    var path = document.querySelector('.path')
+    if (path) {
+      this.pathLength = path.getTotalLength()
+      console.log(this.pathLength)
+      // TODO: somehow set this to all values that are currently 3000 (and possibly 100% as well)
+    }
 
-    let l = this.path.node().getTotalLength()
-    this.maxSpeed = l * this.SPEED_MULTIPLIER
-  }
 
-  // Returns an attrTween for translating along the specified path element.
-  translateAlong(path) {
-    var l = path.getTotalLength();
-    return function() {
-      return function(t) {
-        var p = path.getPointAtLength(t * l);
-        return "translate(" + p.x + "," + p.y + ")";
-      };
-    };
-  }
+    var element = document.getElementById('animated-path');
+    if (element) {
+      // TODO: remove these listeners at some point
+      element.addEventListener('animationstart', this.listener, false)
+      element.addEventListener('animationend', this.listener, false)
+      element.addEventListener('animationiteration', this.listener, false)
+    }
+    // this.path = d3.select('path')
 
-  transition() {
-    this.circle.transition()
-      .duration(this.maxSpeed)
-      .attrTween("transform", this.translateAlong(this.path.node()))
-      .ease(d3.easeLinear)
-      .on("end", this.transition)
+    // let l = this.path.node().getTotalLength()
+    // this.maxSpeed = l * this.SPEED_MULTIPLIER
   }
 }
 </script>
 
 <style scoped lang="scss">
-circle {
-  fill: steelblue;
-  stroke: #fff;
-  stroke-width: 5px;
+.path {
+  stroke-dasharray: var(--path-length);
+  stroke-dashoffset: var(--path-length);
+
+  // The animation shorthand CSS property applies an animation between styles. It is a shorthand for animation-name, animation-duration, animation-timing-function, animation-delay, animation-iteration-count, animation-direction, animation-fill-mode, and animation-play-state
+  animation: dash 5s linear forwards; // TODO: set duration programatically
+}
+
+.marker {
+  animation: move 5s linear forwards;
+}
+
+@keyframes move {
+  100% {
+    motion-offset: 100%;
+    offset-distance: 100%;
+  }
+}
+
+@keyframes dash {
+  from {
+    stroke-dashoffset: var(--path-length);
+  }
+  to {
+    stroke-dashoffset: 0;
+  }
 }
 </style>
