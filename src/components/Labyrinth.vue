efdd8925accbea33a77765696195d7f71d54264d
Shawn Taylor <shawn@mapsandapps.net>
2019-12-13 22:41:54 -0500

    :art: Step 2 of store refactor

--- src/components/Labyrinth.vue
+++ src/components/Labyrinth.vue
@@ -6,8 +6,6 @@
       v-if="this.level.path"
       @mousedown="onMousedown"
       @mouseup="onMouseup"
-      @mouseleave="onMouseleave"
-      @mouseout="onMouseout"
       @mousemove="onMousemove">
       <g
         class="path-container"
@@ -58,7 +56,6 @@
       </g>
     </svg>
     <span v-if="finished">Done!</span>
-    <!-- <button id="button" @click="transition">Restart</button> -->
   </div>
 </template>
 
@@ -68,11 +65,6 @@ import { bind, throttle } from 'lodash'
 import { mapGetters, mapState } from 'vuex'
 import store from '../store'
 
-type Point = {
-  x: number
-  y: number
-}
-
 @Component({
   computed: {
     ...mapGetters({
@@ -81,230 +73,72 @@ type Point = {
     }),
     ...mapState('movement', {
       animatedPathStyles: 'animatedPathStyles',
-      circlePositionWithinLabyrinth: 'circlePositionWithinLabyrinth',
-      currentHeading: 'currentHeading',
-      currentSpeed: 'currentSpeed',
-      currentTouchDirection: 'currentTouchDirection',
       debugHeading: 'debugHeading',
       debugMode: 'debugMode',
       debugTouchDirection: 'debugTouchDirection',
       finished: 'finished',
-      lastPassedPoint: 'lastPassedPoint',
-      moving: 'moving',
       pathContainerTransform: 'pathContainerTransform',
-      pathElement: 'pathElement',
-      pathLength: 'pathLength',
-      position: 'position',
-      started: 'started'
+      windowHeight: 'windowHeight',
+      windowWidth: 'windowWidth'
     })
   }
 })
 
 export default class Labyrinth extends Vue {
-  windowWidth = window.innerWidth
-  windowHeight = window.innerHeight
-
-  DURATION_MULTIPLIER: number = 10
-
-  animate() {
-    if (this.position >= this.pathLength) {
-      store.commit('movement/setMoving', false)
-      this.advanceToNextLevel()
-    } else {
-      if (this.moving) {
-        store.commit('movement/setPosition', this.position + 3 * this.currentSpeed)
-        this.setStyles()
-        this.getContainerPosition()
-        window.requestAnimationFrame(this.animate)
-      }
-    }
-  }
-
-  advanceToNextLevel() {
-    console.warn('complete!')
-
-    // @ts-ignore
-    document.getElementById('animated-path').removeEventListener('animationend', this.onAnimationEnd, false)
-
-    this.beginLevel()
-  }
-
-  beginAnimation() {
-    store.commit('movement/setStarted', true)
-    store.commit('movement/setMoving', true)
-
-    this.animate()
-  }
-
-  endAnimation() {
-    store.commit('movement/setMoving', false)
-  }
-
-  setStyles() {
-    // @ts-ignore
-    store.commit('movement/setCirclePositionWithinLabyrinth', this.pathElement.getPointAtLength(this.position))
-
-    const animatedPathStyles = `
-      display: ${this.started ? 'initial' : 'none'};
-      --path-length: ${this.pathLength};
-      stroke-dashoffset: ${this.pathLength - this.position};
-    `
-    store.commit('movement/setAnimatedPathStyles', animatedPathStyles)
-
-    if (this.circlePositionWithinLabyrinth) {
-      const pathContainerTransform = `translate(-${this.circlePositionWithinLabyrinth.x},-${this.circlePositionWithinLabyrinth.y})`
-      store.commit('movement/setPathContainerTransform', pathContainerTransform)
-    }
-  }
-
-  /**
-   * Calculates the direction the circle is "moving" along the line, where 0 is up, 90 is right, etc.
-   * Also used to calculate the position of the cursor/finger relative to the circle
-   */
-  calculateHeading(firstX: number, firstY: number, secondX: number, secondY: number): number {
-    const radians = Math.atan2(firstX - secondX, secondY - firstY)
-    const degrees = radians * (180 / Math.PI)
-    return degrees < 0 ? 360 + degrees : degrees
-  }
-
-  getContainerPosition(): void {
-    // NOTE: needs to be called after setStyles() so circlePositionWithinLabyrinth will be set
-    if(this.moving && this.circlePositionWithinLabyrinth) {
-      const rect = this.circlePositionWithinLabyrinth
-      if (this.lastPassedPoint) {
-        let debugHeading = `M ${this.windowWidth / 2},${this.windowHeight / 2} l ${(rect.x - this.lastPassedPoint.x) * 100},${(rect.y - this.lastPassedPoint.y) * 100}`
-        store.commit('movement/setDebugHeading', debugHeading)
-
-        const distanceBetweenCurrentAndLast = Math.hypot(rect.x - this.lastPassedPoint.x, rect.y - this.lastPassedPoint.y)
-        if (distanceBetweenCurrentAndLast > 1) {
-          const currentHeading = this.calculateHeading(this.lastPassedPoint.x, this.lastPassedPoint.y, rect.x, rect.y)
-          store.commit('movement/setCurrentHeading', currentHeading)
-
-          this.calculateSpeed()
-          store.commit('movement/setLastPassedPoint', this.circlePositionWithinLabyrinth)
-        }
-      } else {
-        store.commit('movement/setCurrentSpeed', 1)
-        store.commit('movement/setLastPassedPoint', this.circlePositionWithinLabyrinth)
-      }
-    }
-  }
-
   onTouchstart(): void {
-    this.beginAnimation()
+    store.dispatch('movement/beginAnimation')
   }
 
   onMousedown(): void {
-    this.beginAnimation()
+    store.dispatch('movement/beginAnimation')
   }
 
   onTouchend(): void {
-    this.endAnimation()
+    store.dispatch('movement/endAnimation')
   }
 
   onMouseup(): void {
-    this.endAnimation()
+    store.dispatch('movement/endAnimation')
   }
-
-  onMouseout() {}
-
-  onMouseleave() {}
 
   // @ts-ignore
   onTouchmove(e) {
     // @ts-ignore
     e.preventDefault()
     // TODO: might need to throttle this? it sounds like chrome may throttle to 200ms by default
-    this.getCurrentTouchDirection(e.touches[0].pageX, e.touches[0].pageY)
+    store.dispatch('movement/getCurrentTouchDirection', {
+      x: e.touches[0].pageX,
+      y: e.touches[0].pageY
+    })
   }
 
-  onMousemove = bind(throttle(e => this.getCurrentTouchDirection(e.x, e.y), 50, {
+  onMousemove = bind(throttle(e => {
+    store.dispatch('movement/getCurrentTouchDirection', {
+      x: e.x,
+      y: e.y
+    })
+  }, 50, {
     leading: true
   }), this)
-
-  calculateSpeed(): void {
-    if (typeof this.currentHeading === 'number' && typeof this.currentTouchDirection === 'number') {
-      const inverseSpeed = Math.abs(this.currentHeading - this.currentTouchDirection) // 0 to 180
-
-      store.commit('movement/setCurrentSpeed', Math.max(90 - inverseSpeed, 0) / 90) // 0 to 1
-    }
-  }
-
-  getCurrentTouchDirection(x: number, y: number): void {
-    // TODO: probably should also call this on mousedown
-    // TODO: can probably store position of circle: it shouldn't change
-    const circle = document.querySelector('.position')
-    if (circle) { // FIXME: how is there ever not a circle? 🤔
-      const circleRect = circle.getBoundingClientRect()
-      const circlePosition: Point = {
-        x: circleRect.left + (circleRect.width / 2),
-        y: circleRect.top + (circleRect.height / 2)
-      }
-
-      const currentTouchDirection = this.calculateHeading(circlePosition.x, circlePosition.y, x, y)
-      store.commit('movement/setCurrentTouchDirection', currentTouchDirection)
-
-      const debugTouchDirection = `M ${this.windowWidth / 2},${this.windowHeight / 2} l ${x - circlePosition.x},${y - circlePosition.y}`
-      store.commit('movement/setDebugTouchDirection', debugTouchDirection)
-
-      this.calculateSpeed()
-    }
-  }
 
   // @ts-ignore
   onAnimationEnd(e) {
     // @ts-ignore
     if (e.type === 'animationend') {
-      store.commit('movement/setFinished', true)
-      store.commit('movement/setLastPassedPoint', null)
-      store.commit('movement/setCurrentHeading', null)
-      store.commit('movement/setCurrentTouchDirection', null)
-      store.commit('movement/setCurrentSpeed', 1)
+      store.dispatch('movement/stopMovement')
     }
   }
 
   onWindowResize(): void {
-    this.windowWidth = window.innerWidth
-    this.windowHeight = window.innerHeight
-  }
-
-  setupLevel() {
-    if (process.env.NODE_ENV === 'development') {
-      store.commit('movement/setDebugMode', true)
-    }
-
-    // TODO: refactor this with onAnimationEnd (some repeats)
-    store.commit('movement/setPosition', 0)
-    store.commit('movement/setMoving', false)
-    store.commit('movement/setStarted', false)
-    store.commit('movement/setFinished', false)
-    store.commit('movement/setLastPassedPoint', null)
-    store.commit('movement/setCurrentHeading', null)
-    store.commit('movement/setCurrentTouchDirection', null)
-
-    // @ts-ignore
-    store.commit('movement/setPathElement', document.getElementById('animated-path'))
-    // @ts-ignore
-    store.commit('movement/setPathLength', this.pathElement.getTotalLength())
-
-    // @ts-ignore
-    this.pathElement.addEventListener('animationend', this.onAnimationEnd, false)
-
-    this.setStyles()
-  }
-
-  beginLevel() {
-    store.commit('setCurrentLevel')
-
-    setTimeout(bind(function(){
-    // wait for svg to paint the new level
-    // TODO: make this longer (just in case) and do some animation or something to hide it
-    // @ts-ignore
-      this.setupLevel()
-    }, this), 100)
+    store.commit('movement/setWindowSize', {
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
   }
 
   mounted() {
+    this.onWindowResize()
+
     document.body.addEventListener('touchstart', this.onTouchstart, false)
     document.body.addEventListener('touchmove', this.onTouchmove, {
       passive: false
@@ -312,7 +146,7 @@ export default class Labyrinth extends Vue {
     document.body.addEventListener('touchend', this.onTouchend, false)
     window.addEventListener('resize', this.onWindowResize)
 
-    this.beginLevel()
+    store.dispatch('movement/beginLevel')
   }
 
   beforeDestroy() {
--- src/store/movement.ts
+++ src/store/movement.ts
@@ -1,7 +1,26 @@
+import { bind } from 'lodash'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+
+const SPEED_MULTIPLIER = 3
+
+type Point = {
+  x: number
+  y: number
+}
+
+/**
+ * Calculates the direction the circle is "moving" along the line, where 0 is up, 90 is right, etc.
+ * Also used to calculate the position of the cursor/finger relative to the circle
+ */
+// const calculateHeading: number = (firstX: number, firstY: number, secondX: number, secondY: number) => {
+const calculateHeading = (firstX: number, firstY: number, secondX: number, secondY: number) => {
+  const radians = Math.atan2(firstX - secondX, secondY - firstY)
+  const degrees = radians * (180 / Math.PI)
+  return degrees < 0 ? 360 + degrees : degrees
+}
 
 // TODO: types
 export default {
@@ -22,7 +41,9 @@ export default {
     pathElement: null, // Element | null
     pathLength: 0, // number
     position: 0, // number
-    started: false
+    started: false,
+    windowHeight: 0,
+    windowWidth: 0
   },
   getters: {
   },
@@ -74,9 +95,150 @@ export default {
     },
     setStarted(state, payload) {
       state.started = payload
+    },
+    setWindowSize(state, payload) {
+      state.windowWidth = payload.width
+      state.windowHeight = payload.height
     }
   },
   actions: {
+    advanceToNextLevel({ commit, dispatch, state }) {
+      console.warn('complete!')
+      commit('setMoving', false)
+
+      // @ts-ignore
+      document.getElementById('animated-path').removeEventListener('animationend', state.onAnimationEnd, false)
+
+      dispatch('beginLevel')
+    },
+    animate({ commit, dispatch, state }) {
+      const recurse = () => {
+        dispatch('animate')
+      }
+
+      if (state.position >= state.pathLength) {
+        dispatch('advanceToNextLevel')
+      } else {
+        if (state.moving) {
+          commit('setPosition', state.position + SPEED_MULTIPLIER * state.currentSpeed)
+          dispatch('setStyles')
+          dispatch('getContainerPosition')
+          window.requestAnimationFrame(recurse)
+        }
+      }
+    },
+    beginAnimation({ commit, dispatch }) {
+      commit('setStarted', true)
+      commit('setMoving', true)
+
+      dispatch('animate')
+    },
+    beginLevel({ commit, dispatch }) {
+      commit('setCurrentLevel', null, { root: true })
+
+      setTimeout(bind(function(){
+        // wait for svg to paint the new level
+        // TODO: make this longer (just in case) and do some animation or something to hide it
+        dispatch('setupLevel')
+      }, this), 100)
+    },
+    calculateSpeed({ commit, state }) {
+      // FIXME: state.currentHeading is always null
+      if (typeof state.currentHeading === 'number' && typeof state.currentTouchDirection === 'number') {
+        const inverseSpeed = Math.abs(state.currentHeading - state.currentTouchDirection) // 0 to 180
+
+        commit('setCurrentSpeed', Math.max(90 - inverseSpeed, 0) / 90) // 0 to 1
+      }
+    },
+    // FIXME: these should probably be renamed something not beginning in 'get'
+    getContainerPosition({ commit, dispatch, state }) {
+      // NOTE: needs to be called after setStyles() so circlePositionWithinLabyrinth will be set
+      if(state.moving && state.circlePositionWithinLabyrinth) {
+        const rect = state.circlePositionWithinLabyrinth
+        if (state.lastPassedPoint) {
+          let debugHeading = `M ${state.windowWidth / 2},${state.windowHeight / 2} l ${(rect.x - state.lastPassedPoint.x) * 100},${(rect.y - state.lastPassedPoint.y) * 100}`
+          commit('setDebugHeading', debugHeading)
+
+          const distanceBetweenCurrentAndLast = Math.hypot(rect.x - state.lastPassedPoint.x, rect.y - state.lastPassedPoint.y)
+          if (distanceBetweenCurrentAndLast > 1) {
+            const currentHeading = calculateHeading(state.lastPassedPoint.x, state.lastPassedPoint.y, rect.x, rect.y)
+            commit('setCurrentHeading', currentHeading)
+
+            dispatch('calculateSpeed')
+            commit('setLastPassedPoint', this.circlePositionWithinLabyrinth)
+          }
+        } else {
+          commit('setCurrentSpeed', 1)
+          commit('setLastPassedPoint', this.circlePositionWithinLabyrinth)
+        }
+      }
+    },
+    getCurrentTouchDirection({ commit, dispatch, state }, touchPosition: Point) {
+      // TODO: probably should also call this on mousedown
+      // TODO: can probably store position of circle: it shouldn't change
+      const circle = document.querySelector('.position')
+      if (circle) { // FIXME: how is there ever not a circle? 🤔
+        const circleRect = circle.getBoundingClientRect()
+        const circlePosition: Point = {
+          x: circleRect.left + (circleRect.width / 2),
+          y: circleRect.top + (circleRect.height / 2)
+        }
+
+        const currentTouchDirection = calculateHeading(circlePosition.x, circlePosition.y, touchPosition.x, touchPosition.y)
+        commit('setCurrentTouchDirection', currentTouchDirection)
+
+        const debugTouchDirection = `M ${state.windowWidth / 2},${state.windowHeight / 2} l ${touchPosition.x - circlePosition.x},${touchPosition.y - circlePosition.y}`
+        commit('setDebugTouchDirection', debugTouchDirection)
+
+        dispatch('calculateSpeed')
+      }
+    },
+    endAnimation({ commit }) {
+      commit('setMoving', false)
+    },
+    setStyles({ commit, state }) {
+      // @ts-ignore
+      commit('setCirclePositionWithinLabyrinth', state.pathElement.getPointAtLength(state.position))
+
+      const animatedPathStyles = `
+        display: ${state.started ? 'initial' : 'none'};
+        --path-length: ${state.pathLength};
+        stroke-dashoffset: ${state.pathLength - state.position};
+      `
+      commit('setAnimatedPathStyles', animatedPathStyles)
+
+      if (state.circlePositionWithinLabyrinth) {
+        const pathContainerTransform = `translate(-${state.circlePositionWithinLabyrinth.x},-${state.circlePositionWithinLabyrinth.y})`
+        commit('setPathContainerTransform', pathContainerTransform)
+      }
+    },
+    setupLevel({ commit, dispatch, state }) {
+      if (process.env.NODE_ENV === 'development') {
+        commit('setDebugMode', true)
+      }
+
+      commit('setPosition', 0)
+      commit('setMoving', false)
+      commit('setStarted', false)
+      dispatch('stopMovement')
+
+      // @ts-ignore
+      commit('setPathElement', document.getElementById('animated-path'))
+      // @ts-ignore
+      commit('setPathLength', state.pathElement.getTotalLength())
+
+      // @ts-ignore
+      state.pathElement.addEventListener('animationend', state.onAnimationEnd, false)
+
+      dispatch('setStyles')
+    },
+    stopMovement({ commit }) {
+      commit('setFinished', true)
+      commit('setLastPassedPoint', null)
+      commit('setCurrentHeading', null)
+      commit('setCurrentTouchDirection', null)
+      commit('setCurrentSpeed', 1)
+    }
   },
   modules: {
   }
